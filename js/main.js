6cb59e67f4b302d9d9ab00317fc750631e6b628c
dova <danijeldoveden.55@gmail.com>
2024-10-11 18:40:09 +0200

    sredjen

--- js/main.js
+++ js/main.js
@@ -1,83 +1,79 @@
-$(() => {
-    $('div.navbar-collapse ul li a').click(() => {
-        $('div.navbar-collapse').removeClass("show");
-        $("img.visible").removeClass('visible');
-        $("button span img").attr("src", "img/bars2.png");
+
+function getYear() {
+    var currentDate = new Date();
+    var currentYear = currentDate.getFullYear();
+    document.querySelector("#displayYear").innerHTML = currentYear;
+}
+var navbar = $("#navbar");
+var ww = $(window).width();
+var wh = $(window).height();
+getYear();
+var isMobile;
+if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+    isMobile = true;
+}
+console.log(wh)
+if (isMobile) {
+    if (hero + 100 < window.scrollY) {
+        navbar.addClass('show');
+    }
+}
+// toggler button
+$(".toggler").click(function () {
+    $(".nav-links").toggleClass("active");
+    $(".nav-link").click(function () {
+        $(".nav-links").removeClass("active");
+        $(".toggler").removeClass("hide");
     });
-    window.onscroll = function() {
-        navbarFunction();
-        dodajAnimu();
-    }
-
-    function navbarFunction() {
-        if (document.documentElement.scrollTop > 70) {
-            var navbar = document.getElementById('my-navbar');
-            navbar.classList.add('my-fixed-top');
-            navbar.style.opacity = "1";
-            var section = document.getElementsByClassName('hero-sec')[0].style.marginTop = '0';
+    $(".toggler").toggleClass("hide");
+});
+$("#services ul li").click((e) => {
+    $.each($(".srvtxt"), function () { $(this).css("display", "none") });
+    $.each($(".srvtxt"), function () {
+        if ($(e.target).attr('target') === $(this).attr("id")) {
+            $(this).css("display", "flex");
         }
-    }
-    var toggler = document.getElementById('my-toggler');
-    var navbar = document.getElementById('my-nav');
-
-    toggler.addEventListener('click', () => {
-
-        toggler.getElementsByTagName('img')[0].classList.toggle('visible');
-        $("button span img").attr("src", "img/bars2.png");
-        $("img.visible").attr("src", "img/znak.png");
     });
-    $("#services ul li").click((e) => {
-        $.each($(".tekst"), function() { $(this).css("display", "none") });
-        $.each($(".tekst"), function() {
-            if ($(e.target).attr('target') === $(this).attr("id")) {
-                $(this).css("display", "block");
-            }
-        });
-    });
+});
 
-    var isMobile;
-    if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
-        isMobile = true;
-    }
+$('#usluges').on('change', () => {
+    $(".srvtxt").each(function () {
+        if ($("select option:selected").val() === $(this).attr("id"))
+            $(this).css("display", "block");
+        else
+            $(this).css("display", "none");
+    })
 
-    $("#hero .ml-auto>img").css("opacity", "1");
-    var windowHeight = (window.innerHeight * 5) / 10;
-    var services = document.getElementById("services");
-    var law = services.getElementsByClassName("col-12 col-md-8")[0];
-    var about = document.getElementById("about");
-    var contact = document.getElementById("contact");
-    var footer = document.getElementsByTagName("footer")[0];
-
-
-    function dodajAnimu() {
-        var positionS = services.getBoundingClientRect().top;
-        var positionL = law.getBoundingClientRect().top;
-        var positionA = about.getBoundingClientRect().top;
-        var positionC = contact.getBoundingClientRect().top;
-        var positionF = footer.getBoundingClientRect().top;
-
-
-        if (windowHeight >= positionS) {
-            services.getElementsByTagName("h2")[0].classList.add("anima-up-down");
-            services.getElementsByTagName("img")[0].classList.add("small-in");
-            services.getElementsByClassName("row")[0].classList.add("anima-left");
-        }
-        if (windowHeight >= positionL) {
-            law.classList.add("small-in");
-        }
-        if (windowHeight >= positionA) {
-            about.getElementsByClassName("row")[0].classList.add("anima-left");
-            about.getElementsByTagName("img")[0].classList.add("small-in");
-            about.getElementsByTagName("h2")[0].classList.add("anima-up-down");
-        }
-        if (windowHeight >= positionC) {
-            contact.getElementsByTagName("h2")[0].classList.add("anima-up-down");
-            contact.getElementsByClassName("col-12 col-md-6")[0].classList.add("anima-right");
-            contact.getElementsByClassName("col-12 col-md-6")[1].classList.add("anima-right");
-        }
-        if (window.innerHeight - 100 >= positionF) {
-            contact.getElementsByClassName("col-12 m-auto")[0].classList.add("anima-down-up");
-            footer.getElementsByClassName("row no-gutters flex-column text-center")[0].classList.add("anima-left");
-        }
-    }
 })
+$("#hero .ml-auto>img").css("opacity", "1");
+var windowHeight = (window.innerHeight * 5) / 10;
+var oblast = document.getElementById("oblast");
+var about = document.getElementById("about");
+
+
+function dodajAnimu() {
+    var oblastP = oblast.getBoundingClientRect().top;
+    var positionA = about.getBoundingClientRect().top;
+    if (wh >= oblastP) {
+        oblast.getElementsByTagName("h2")[0].classList.add("anima-up-down");
+        oblast.getElementsByTagName("img")[0].classList.add("small-in");
+        oblast.getElementsByClassName("row")[0].classList.add("anima-left");
+    }
+    if (wh >= positionA) {
+        about.getElementsByClassName("row")[0].classList.add("anima-left");
+        about.getElementsByTagName("img")[0].classList.add("small-in");
+        about.getElementsByTagName("h2")[0].classList.add("anima-up-down");
+    }
+}
+function navbarAnima() {
+    if (wh / 2 < window.scrollY) {
+        navbar.addClass('show');
+    } else {
+        navbar.removeClass('show');
+    }
+}
+
+$(window).on('scroll', () => {
+    navbarAnima();
+    dodajAnimu();
+});
